db2d7a2976663f0e158c153f6cbeb02ce4a58652
Hugo Rossignol <rossignh@tcd.ie>
2023-10-11 13:01:20 +0100

    Added C code to perform perform simulation and cleaned directory.

---
+++ charge2mass.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <uxhw.h>
+
+int main(int argc, char *  argv[])
+{
+	double	I, V, R, ratio;
+
+	I = UxHwDoubleGaussDist(1.4, 0.1);
+	printf("I = %lf\n", I);
+
+	V = UxHwDoubleGaussDist(82.2,2.0);
+	printf("V = %lf\n", V);
+
+	R = UxHwDoubleGaussDist(0.0292,0.0011);
+	printf("R = %lf\n", R);
+
+    double k = 0.00075 ;
+
+    ratio = (2*V)/(k*k*I*I*R*R);
+    printf("ratio = %lf\n", ratio);
+
+#ifdef DEBUG
+	printf("debug message\n");
+#endif
+
+	return 0;
+}
